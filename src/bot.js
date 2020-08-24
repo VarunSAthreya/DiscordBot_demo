8dfb34bf2403bfaa09f92f1f663c5459332647bc
Varun S Athreya <varun.sathreya@gmail.com>
2020-08-25 04:00:37 +0530

    Created kick command

---
+++ src/bot.js
@@ -0,0 +1,35 @@
+require("dotenv").config();
+const { Client } = require("discord.js");
+
+const PREFIX = "$";
+const client = new Client();
+
+client.on("ready", () => {
+    console.log(`${client.user.username} has logged in`);
+});
+
+client.on("message", (message) => {
+    if (message.author.bot) return;
+
+    if (message.content.startsWith(PREFIX)) {
+        const [CMD_NAME, ...args] = message.content
+            .trim()
+            .substring(PREFIX.length)
+            .split(/\s+/); // REGEX to prevent duplicate spaces
+
+        if (CMD_NAME === "kick") {
+            if (args.length === 0) return message.reply("Please provide an ID");
+
+            const member = message.guild.members.cache.get(args[0]);
+
+            if (member) {
+                member.kick();
+                message.channel.send(`${member.user.tag} has been kicked!`);
+            } else {
+                message.channel.send("Member was not found!");
+            }
+        }
+    }
+});
+
+client.login(process.env.DISCORDJS_BOT_TOKEN);
